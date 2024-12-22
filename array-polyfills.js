86631f109d19afbc58ca6aba32c7b0789fe7f6bf
Mona Lisa <hpandey402@gmail.com>
2024-12-22 13:05:06 +0530

    Adding polyfills for Array

---
+++ array-polyfills.js
@@ -0,0 +1,66 @@
+//Array-Polyfills
+
+//ForEach
+Array.prototype.myForEach = function (cb) {
+  for (let i = 0; i < this.length; i++) {
+    cb(this[i], i, this);
+  }
+};
+//Map
+Array.prototype.myMap = function (cb) {
+  const newArr = [];
+  for (let i = 0; i < this.length; i++) {
+    newArr.push(cb(this[i], i, this));
+  }
+  return newArr;
+};
+//Filter
+Array.prototype.myFilter = function (cb) {
+  const newArr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) {
+      newArr.push(this[i]);
+    }
+  }
+  return newArr;
+};
+
+//Find
+Array.prototype.myFind = function (cb) {
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) {
+      return this[i];
+    }
+  }
+  return undefined;
+};
+
+//Some
+Array.prototype.mySome = function (cb) {
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// //Every
+Array.prototype.myEvery = function (cb) {
+  for (let i = 0; i < this.length; i++) {
+    if (!cb(this[i], i, this)) return false;
+  }
+  return true;
+};
+
+//Reduce
+Array.prototype.myReduce = function (cb, initialValue) {
+  for (let i = 0; i < this.length; i++) {
+    initialValue = cb(initialValue, this[i], i, this);
+  }
+  return initialValue;
+};
+
+const bcd = ['abc', 'bd', '78'].myReduce((acc, el) => acc + el, 0);
+
+console.log(bcd);
